1328e135d4bedc92570a5ba8d1b518c3adea0a74
Jenna Murrell <murrelljenna@gmail.com>
2021-01-10 14:40:38 -0500

    Added testing of auth-token route.

--- apps/tenant_db/models.py
+++ apps/tenant_db/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.core.validators import EmailValidator
-from django.core.validators import RegexValidator
-
 
 class Building(models.Model):
     street_number = models.CharField(max_length=30)
--- apps/tenant_db/tests.py
+++ apps/tenant_db/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class AuthenticationTest(TestCase):
+    """ For testing authentication routes and token provisioning """
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.token_response = self.client.post('/token-auth/', {'username': self.user.username, 'password': '12345'})
+        pass
+
+    def test_acquire_token(self):
+        """Supplying correct credentials to token route should return a JWT"""
+        self.assertEqual(self.token_response.status_code, 200)
+        self.assertIn('access', self.token_response.data)
+        self.assertIn('refresh', self.token_response.data)
+        pass
+
+    def test_validate_token(self):
+        response = self.client.get('/contacts/', HTTP_AUTHORIZATION=f"Bearer {self.token_response.data['access']}")
+        self.assertEqual(response.status_code, 200)
+        pass
