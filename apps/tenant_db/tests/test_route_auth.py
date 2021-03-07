587d8473091d3851797c9d1045a359c8dff7102c
Jenna Murrell <murrelljenna@gmail.com>
2021-03-07 16:53:49 -0500

    Added contacts test route
    Thinking about what testing I should bother with for the backend. Pretty thin backend.

--- apps/tenant_db/tests/test_route_auth.py
+++ apps/tenant_db/tests/test_route_auth.py
@@ -5,14 +5,21 @@ class AuthenticationRouteTest(TestCase):
     """ For testing authentication routes and token provisioning """
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
-        self.token_response = self.client.post('/token-auth/', {'username': self.user.username, 'password': '12345'})
-
-    def test_acquire_token(self):
+    def test_acquire_token_success(self):
         """Supplying correct credentials to token route should return a JWT"""
-        self.assertEqual(self.token_response.status_code, 200)
-        self.assertIn('access', self.token_response.data)
-        self.assertIn('refresh', self.token_response.data)
+        token_response = self.client.post('/token-auth/', {'username': self.user.username, 'password': '12345'})
+
+        self.assertEqual(token_response.status_code, 200)
+        self.assertIn('access', token_response.data)
+        self.assertIn('refresh', token_response.data)
+
+    def test_acquire_token_fail(self):
+        token_response = self.client.post('/token-auth/', {'username': self.user.username, 'password': 'invalid_password'})
+
+        self.assertEqual(token_response.status_code, 401)
+       
 
     def test_validate_token(self):
-        response = self.client.get('/contacts/', HTTP_AUTHORIZATION=f"Bearer {self.token_response.data['access']}")
+        token_response = self.client.post('/token-auth/', {'username': self.user.username, 'password': '12345'})
+        response = self.client.get('/contacts/', HTTP_AUTHORIZATION=f"Bearer {token_response.data['access']}")
         self.assertEqual(response.status_code, 200)
--- apps/tenant_db/tests/test_route_buildings.py
+++ apps/tenant_db/tests/test_route_buildings.py
@@ -1,5 +1,23 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from apps.tenant_db.models import Contact
+
+def getAuthorizedClient(username, password):
+    token = Client().post('/token-auth/', {'username': username, 'password': password})
+    token_header = f"Bearer {token.data['access']}"
+    return Client(HTTP_AUTHORIZATION=token_header)
 
 class BuildingsRouteTest(TestCase):
-    pass
+    def setUp(self):
+        username = 'testuser'
+        password = '12345'
+        self.user = User.objects.create_user(username=username, password=password)
+        self.client = getAuthorizedClient(username, password)
+
+    def test_get_contacts(self):
+        Contact.objects.create(
+            first_name = "KRS",
+            last_name = "-One",
+        )
+        response = self.client.get('/contacts/')
+        self.assertEqual(len(response.data), 1)
